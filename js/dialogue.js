9f45cf7286fc4cff9a2650bcf322e529fb093115
Driek van der Meulen <driekvandermeulen@icloud.com>
2023-12-30 11:38:50 +0100

    dialogue

---
+++ js/dialogue.js
@@ -0,0 +1,3 @@
+//take .dialogueText and replace it with the dialogue from ./json/dialogue.json use the dialogue id to find the correct dialogue. start at 0. create a command to move to the next dialogue.
+// Path: js/commands.js
+//take .commandText and replace it with the command from ./json/commands.json use the command id to find the correct command. start at 0. create a command to move to the next command.
