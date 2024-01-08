a11f015122d2fb8184a92a2707d9e65cab8e6c54
Driek van der Meulen <driekvandermeulen@icloud.com>
2024-01-08 10:02:06 +0000

    alpha 0.21
    Current version alpha 0.3 Dialogue Script version: beta 0.2
    
    Changelog alpha 0.21 (08/01/2024 10am GMT)
    
    * Added bug report
    * Changes to start screen to go to a timed based introduction
    * Changes to fonts to better match prototype
    * Changes to styling to better match prototype
    *
    * Current known bugs:
    * Agency font doesn’t work
    * Startscreen background doesn’t work
    * Previous button functions as next button
    
    Upcomming features:
    * changes to start screen to go to a timed based introduction
    * changes to side windows to better match prototype
    * changes to fonts to better match prototype
    * changes to styling to better match prototype
    * fix bug that prevents the background on the start screen
    
    Changelog alpha 0.2 (07/01/2024)
    * Added side windows (non functional)
    * Added main menu page
    * Added visuals to the aplication
    * Added styling to dialogue box
    * Added node modules (Typescript)
    * Changes to json, updated to alpha 0.2 dialogue script
    * Changes to json, added variables to use in the aplication
    
    Changelog alpha 0.1 (23/12/2023)
    * Created package
    * Added Node Modules (SCSS)
    * Added alpha dialogue script aplha 0.1
    * Added dialogue box
    * Added Bruce / Nova check
    * Upcoming features are features that are already in the pipeline and currently worked on but not ready to push to a prod enviroment

--- js/home.js
+++ js/home.js
@@ -1,21 +1,45 @@
 let counter = 0;
 
+// CSS animation class
+const animationClass = "title-animate";
+
 function handleEnterKeyPress(event) {
   if (event.key === "Enter") {
-    let titleElement = document.querySelector(".title");
-
-    if (titleElement) {
-      titleElement.textContent = "In samenwerking met zuyd hogeschool";
-      counter++;
-      checkCounterAndManipulate();
-    }
+    manipulateTitle();
   }
 }
 
 document.addEventListener("keydown", handleEnterKeyPress);
 
 function checkCounterAndManipulate() {
+  // Implement your logic here for when counter === 2
   if (counter === 2) {
+    // Example: console.log('Counter is 2');
+  }
+}
+
+// Function to manipulate the title and apply animation
+function manipulateTitle() {
+  let titleElement = document.querySelector(".title");
+
+  if (titleElement) {
+    titleElement.innerHTML =
+      'In samenwerking met <span class="bold">Centraal Bureau voor de Statistiek</span>';
+    titleElement.classList.add(animationClass);
+    titleElement.addEventListener("animationend", () => {
+      titleElement.classList.remove(animationClass);
+    });
+
+    counter++;
+    checkCounterAndManipulate();
+  }
+}
+
+// Manipulate the DOM every 5 seconds
+function manipulateDOMEvery5Seconds() {
+  setInterval(manipulateTitle, 5000);
+
+  setInterval(function () {
     let h1Element = document.querySelector(".title");
     if (h1Element) {
       h1Element.remove();
@@ -30,6 +54,8 @@ function checkCounterAndManipulate() {
     let text = document.querySelector(".text");
     text.style.right = "4vw";
     let audio = new Audio("../assets/audio/temp.mp3");
-    audio.play();
-  }
+    // audio.play();
+  }, 10000);
 }
+
+manipulateDOMEvery5Seconds();
