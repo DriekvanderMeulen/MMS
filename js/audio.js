b478357a3da2775aab4cc2228f41bb645821ada4
Driek van der Meulen <driekvandermeulen@icloud.com>
2024-01-19 13:24:02 +0100

    Delete audio.js

--- js/audio.js
+++
@@ -1,8 +0,0 @@
-// // Iterate through dialogues and log when audio = true
-// dialogues.forEach(function (dialogue, i) {
-//   if (dialogue.audio) {
-//     console.log(`Audio is true for dialogue[${i}]:`, dialogue.dialogue);
-//   }
-// });
-
-// // Rest of your code...
